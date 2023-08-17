2d7a4b5e511ed65c94d09f3807eb1d261270361c
iVladyuser <korol.v09@gmail.com>
2023-08-17 16:23:16 +0200

    task-1

---
+++ js/01-gallery.js
@@ -0,0 +1,44 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+// console.log(galleryItems);
+
+const container = document.querySelector(".gallery");
+const markup = galleryItems.map(
+	({ preview, original, description }) => `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+    />
+    </a>
+</li>`
+);
+
+container.insertAdjacentHTML("beforeend", markup.join(""));
+container.addEventListener("click", openFullSize);
+
+function openFullSize(evt) {
+	if (!evt.target.classList.contains("gallery__image")) {
+		return;
+	}
+	evt.preventDefault();
+
+	const originalImgLink = evt.target.dataset.source;
+	const instance = basicLightbox.create(`
+    <img src="${originalImgLink}">
+`);
+
+	instance.show();
+
+	document.addEventListener("keydown", (evt) => {
+		if (evt.key === "Escape") {
+			const visible = basicLightbox.visible();
+			if (visible) {
+				instance.close();
+			}
+		}
+	});
+}
